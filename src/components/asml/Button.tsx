4c1693c1234919658c91aaa17f8579b83e12808a
Daniel Strong <danielstrong.ds@gmail.com>
2021-12-06 19:23:01 +0900

    correctly position button

--- src/components/asml/Button.tsx
+++ src/components/asml/Button.tsx
@@ -2,7 +2,7 @@ import { ArrowSquareOut } from "phosphor-react";
 
 export default function Button() {
   return (
-    <button className="bg-accent text-white rounded-full p-3.5 w-72 shadow">
+    <button className="bg-accent text-white rounded-full p-3.5 w-72 shadow fixed bottom-8 inset-x-0 mx-auto">
       <span className="align-middle">Participate</span>
       <ArrowSquareOut className="h-6 w-6 inline-block ml-4" />
     </button>
