483734b1c6f463063e3dfb18a5fa4eaac6b620f7
Daniel Strong <danielstrong.ds@gmail.com>
2021-12-06 21:49:56 +0900

    Update asml.tsx
    Corrected bg color

--- src/pages/asml.tsx
+++ src/pages/asml.tsx
@@ -10,7 +10,7 @@ export default function ASMLPage() {
         <title>ASML | CollAction</title>
       </Head>
 
-      <div className="bg-white">
+      <div className="bg-black-0">
         {/* CHECK THE PADDING-BOTTOM ONCE ALL CONTENT IS ADDED */}
         <div className="px-5 md:px-8 pt-28 pb-14 md:pb-12 lg:pb-1">
           <div className="sm:w-100 mx-auto mb-15 md:mb-18 lg:mb-22">
