3574a28e6f3e386f51bb6da441f5cb8563765f9d
Daniel Strong <danielstrong.ds@gmail.com>
2021-12-15 00:05:31 +0900

    added app preview section, increased copy width and adjusted spacing accordingly

--- src/pages/asml.tsx
+++ src/pages/asml.tsx
@@ -4,6 +4,10 @@ import Image from "next/image";
 import { ArrowSquareOut } from "phosphor-react";
 import ASMLBanner from "public/veganuary-ASML-banner.png";
 import CollActionLogoWithText from "public/black-logo-name.png";
+import AppPreviewCard1 from "public/app-preview-card-1.png";
+import AppPreviewCard2 from "public/app-preview-card-2.png";
+import AppPreviewCard3 from "public/app-preview-card-3.png";
+import AppPreviewCard1NoShadow from "public/Card1noborder.png";
 
 export default function ASMLPage() {
   return (
@@ -12,9 +16,8 @@ export default function ASMLPage() {
         <title>ASML | CollAction</title>
       </Head>
 
-      <div className="bg-black-0">
-        {/* CHECK THE PADDING-BOTTOM ONCE ALL CONTENT IS ADDED */}
-        <div className="px-5 md:px-8 pt-28 pb-14 md:pb-12 lg:pb-1">
+      <div>
+        <div className="bg-black-0 px-5 md:px-8 py-28 md:pb-18">
           <div className="max-w-400 mx-auto mb-15 md:mb-18 lg:mb-22">
             <h1 className="text-black-400 text-center mb-8">
               We connect people to solve collective action problems
@@ -48,7 +51,7 @@ export default function ASMLPage() {
             href="https://forms.office.com/r/haw1WhtRz1"
             target="_blank"
             rel="noreferrer"
-            className="bg-accent font-bold leading-none text-white text-center rounded-full p-3.5 w-72 shadow fixed bottom-8 inset-x-0 mx-auto"
+            className="bg-accent font-bold leading-none text-white text-center rounded-full p-3.5 w-72 shadow fixed bottom-8 inset-x-0 mx-auto z-50"
             aria-label="Participate"
           >
             <span className="align-middle">Participate</span>
@@ -58,7 +61,7 @@ export default function ASMLPage() {
             />
           </a>
 
-          <div className="max-w-400 mx-auto my-12 md:my-15 lg:my-26">
+          <div className="max-w-500 mx-auto mt-12 md:mt-15 lg:mt-26">
             <div className="mb-13">
               <h3 className="mb-8">
                 Hi! Welcome to the ASML edition of Veganuary.
@@ -132,20 +135,84 @@ export default function ASMLPage() {
               </ul>
             </div>
 
-            <div className="mb-13">
+            {/* Can use last variant if we extract this into a card */}
+            {/* https://v2.tailwindcss.com/docs/hover-focus-and-other-states#last-child */}
+            <div className="mb-0">
               <h3 className="mb-8">The impact you’ve made </h3>
               <p className="mb-6">
                 Depending on your commitment you receive a certain badge at the
                 end of the month. We'll calculate your individual impact, as
                 well as that of the whole group!
               </p>
-              <p className="mb-6">
+              <p className="mb-0">
                 Veganuary is a month where we can challenge ourselves. Let's try
                 it out together and help each other by sharing recipes, tips,
                 supplements, and more. Shifting to a more plant-based diet is
                 great for your health and the environment. Want to participate?
                 Click the participate button and fill out the form.
               </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="bg-black-400 px-5 md:px-8 pt-20 pb-24 md:pb-22 text-center">
+          <div className="max-w-500 mx-auto">
+            <h2 className="text-secondary mb-8">
+              The CollAction app is launching soon
+            </h2>
+            <p className="text-black-0 mb-12">
+              We’re working hard on getting the app in the App/Play Store. First
+              the Android build goes live, after which Apple will shortly
+              follow. We’ll let you know at the top of this page when they are
+              live!
+            </p>
+          </div>
+          <div className="flex flex-wrap justify-center lg:justify-start md:max-w-400 lg:max-w-4xl lg:w-208 mx-auto">
+            {/* APP CARD */}
+            <div className="bg-secondary text-black-400 max-w-400 w-full rounded-3xl px-10 lg:px-13 py-8 pb-0 mb-5 md:mb-8 lg:mr-8 overflow-hidden">
+              <h4 className="text-black-400 mb-5">Make a change today</h4>
+              <p className="text-black-200">
+                Join a CrowdAction you want to be part of.
+              </p>
+              <div className="block -mb-14">
+                <Image
+                  src={AppPreviewCard1}
+                  alt="app preview card 1"
+                  layout="responsive"
+                  sizes="(min-width: 769px) 275px, 300px"
+                  placeholder="blur"
+                />
+              </div>
+            </div>
+            <div className="bg-secondary text-black-400 max-w-400 w-full rounded-3xl px-10 lg:px-13 py-8 mb-5 md:mb-8 overflow-hidden">
+              <h4 className="text-black-400 mb-5">
+                Participate at your own pace
+              </h4>
+              <p className="text-black-200 mb-4">
+                Choose your commitments for the CrowdAction.
+              </p>
+              <div className="block">
+                <Image
+                  src={AppPreviewCard2}
+                  alt="app preview card 2"
+                  layout="responsive"
+                  sizes="275px"
+                  placeholder="blur"
+                />
+              </div>
+            </div>
+            <div className="bg-secondary text-black-400 max-w-400 w-full rounded-3xl px-10 lg:px-13 py-8 mb-5 md:mb-8 lg:mr-8 overflow-hidden">
+              <div className="block -mt-14">
+                <Image
+                  src={AppPreviewCard3}
+                  alt="app preview card 3"
+                  layout="responsive"
+                  sizes="275px"
+                  placeholder="blur"
+                />
+              </div>
+              <h4 className="text-black-400 mb-5">Join the wave</h4>
+              <p className="text-black-200">Make an impact together.</p>
             </div>
           </div>
         </div>
