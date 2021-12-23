28f218691dae5f45a66ef665158fdef168c36029
Tom Siebring <tomsiebring@mac.local>
2021-12-23 06:29:23 +0100

    termsofuse instead of termsandconditions

--- src/pages/terms.tsx
+++ src/pages/terms.tsx
@@ -5,16 +5,16 @@ export default function Terms() {
   return (
     <>
       <Head>
-        <title>Terms & Conditions | CollAction</title>
+        <title>Terms of Use | CollAction</title>
       </Head>
 
       <main className="h-screen w-screen relative flex flex-col justify-between py-4 px-0 md:pt-0">
         <div className="container mx-auto">
           <div className="px-4">
             <h1 className="text-collaction text-2xl font-medium">
-              Terms & Conditions
+              Terms of Use
             </h1>
-            <p>Terms & Conditions will be visible here soon.</p>
+            <p>Terms of Use will be visible here soon.</p>
           </div>
         </div>
       </main>
