fdff82e1814b905cc8ad3ca5f2b60663aa4080cc
Tom Siebring <tomsiebring@mac.local>
2021-12-28 12:43:35 +0100

    add impact page

---
+++ src/pages/impact.tsx
@@ -0,0 +1,23 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Terms() {
+  return (
+    <>
+      <Head>
+        <title>Impact | CollAction</title>
+      </Head>
+
+      <main className="h-screen w-screen relative flex flex-col justify-between py-4 px-0 md:pt-0">
+        <div className="container mx-auto">
+          <div className="px-4">
+            <h1 className="text-collaction text-2xl font-medium">
+              Impact Calculation
+            </h1>
+            <p>Calculating your impact based on your commitments can be done here.</p>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
