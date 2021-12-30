bb3b70acc1e8c3c4f2c0c750a10898eb8f94f411
Tom Siebring <tomsiebring@mac.local>
2021-12-30 12:55:45 +0100

    refactor and clean /terms

--- src/pages/privacy.tsx
+++ src/pages/privacy.tsx
@@ -6,12 +6,12 @@ export default function Terms() {
   return (
     <>
       <Head>
-        <title>Privacy Policy | CollAction</title>
+        <title>Privacy and Cookie Policy | CollAction</title>
       </Head>
 
       <main className="bg-black-0 p-5 md:p-12">
         <div className="max-w-600 mx-auto">
-          <h1 className="text-black-400 text-center mb-6">Terms of Use</h1>
+          <h1 className="text-black-400 text-center mb-6">Privacy and Cookie Policy</h1>
           <p className="text-black-400 text-center mb-8">
             version dated December 30th, 2021
           </p>
@@ -25,6 +25,18 @@ export default function Terms() {
             offered by Stichting CollAction (hereinafter referred to as
             "CollAction" or "we" or "us").
           </p>
+
+          <h4 className="text-black-300 mb-8">Terms of Use Platform</h4>
+          <p className="mb-6 last:mb-0">
+            These terms of use, as amended from time to time (hereinafter
+            referred to as the "Terms of Use") describe the terms under which
+            use may be made of the website and the application (together and
+            separately hereinafter also referred to as the "Platform") of and
+            offered by Stichting CollAction (hereinafter referred to as
+            "CollAction" or "we" or "us").
+          </p>
+
+
         </div>
       </main>
     </>
