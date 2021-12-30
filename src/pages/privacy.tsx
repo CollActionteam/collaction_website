1f3379b5f49d5889813b430090fe389f29d4a5b6
Daniel Strong <danielstrong.ds@gmail.com>
2021-12-30 19:41:46 +0900

    feat: init styled privacy page

--- src/pages/privacy.tsx
+++ src/pages/privacy.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import Head from "next/head";
 import Image from "next/image";
 
@@ -8,14 +9,22 @@ export default function Terms() {
         <title>Privacy Policy | CollAction</title>
       </Head>
 
-      <main className="h-screen w-screen relative flex flex-col justify-between py-4 px-0 md:pt-0">
-        <div className="container mx-auto">
-          <div className="px-4">
-            <h1 className="text-collaction text-2xl font-medium">
-              Privacy Policy
-            </h1>
-            <p>Privacy Policy will be visible here soon.</p>
-          </div>
+      <main className="bg-black-0 p-5 md:p-12">
+        <div className="max-w-600 mx-auto">
+          <h1 className="text-black-400 text-center mb-6">Terms of Use</h1>
+          <p className="text-black-400 text-center mb-8">
+            version dated December 30th, 2021
+          </p>
+
+          <h4 className="text-black-300 mb-8">Terms of Use Platform</h4>
+          <p className="mb-6 last:mb-0">
+            These terms of use, as amended from time to time (hereinafter
+            referred to as the "Terms of Use") describe the terms under which
+            use may be made of the website and the application (together and
+            separately hereinafter also referred to as the "Platform") of and
+            offered by Stichting CollAction (hereinafter referred to as
+            "CollAction" or "we" or "us").
+          </p>
         </div>
       </main>
     </>
