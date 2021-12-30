2fd419b8657bc774e6d96dba2be2a59eb572aa81
Tom Siebring <tomsiebring@mac.local>
2021-12-30 12:35:10 +0100

    implement content of Terms

--- src/pages/terms.tsx
+++ src/pages/terms.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import Head from "next/head";
 import Image from "next/image";
 
@@ -8,14 +9,150 @@ export default function Terms() {
         <title>Terms of Use | CollAction</title>
       </Head>
 
-      <main className="h-screen w-screen relative flex flex-col justify-between py-4 px-0 md:pt-0">
-        <div className="container mx-auto">
-          <div className="px-4">
-            <h1 className="text-collaction text-2xl font-medium">
-              Terms of Use
-            </h1>
-            <p>Terms of Use will be visible here soon.</p>
-          </div>
+      <main className="bg-black-0 p-5 md:p-12">
+        <div className="max-w-600 mx-auto">
+          <h1 className="text-black-400 text-center mb-6">Terms of Use</h1>
+          <p className="text-black-400 text-center mb-8">
+            version dated December 30th, 2021
+          </p>
+
+          <h4 className="text-black-300 mb-8">Terms of Use Platform</h4>
+          <p className="mb-6 last:mb-0">
+            These terms of use, as amended from time to time (hereinafter
+            referred to as the (<strong>"Terms of Use"</strong>) describe the terms under which
+            use may be made of the website and the application (together and
+            separately hereinafter also referred to as the <strong>"Platform"</strong>) of and
+            offered by Stichting CollAction (hereinafter referred to as {" "}
+            <strong>"CollAction"</strong> or <strong>"we"</strong> or <strong>"us"</strong>).
+          </p>
+
+          <h4 className="text-black-300 mb-8">Applicability and amendments</h4>
+          <p className="mb-6 last:mb-0">
+          By using the Platform, you (hereinafter also referred to as the <strong>"User"</strong>) agree to these Terms of Use, and these shall apply to your use of the Platform. We reserve the right to unilaterally amend these Terms of Use. We will endeavor to inform you of any (material) changes to the Terms of Use in a timely manner. In addition, we may at any time modify the Platform, change and/or delete data or restrict or discontinue the use of the Platform, without any liability on any grounds whatsoever towards you or other third parties and without you being able to derive any rights therefrom.
+          </p>
+
+          <h4 className="text-black-300 mb-8">Use of the Platform</h4>
+          <p className="mb-6 last:mb-0">
+          The Platform is exclusively intended to give you as a user the opportunity to participate in one or more charity initiatives (CrowdActions and commitments) and/or to start your own charity initiative. It is not allowed to use the Platform for other purposes, such as commercial purposes unless CollAction has provided its prior written consent. It is also not permitted to offer goods or services in exchange for participation in an initiative or for other reasons, unless CollAction has provided its prior written consent. We expressly reject any liability or responsibility for the result of or the success of an initiative. 
+          <br />
+          <br />
+          You are responsible for and are required to use the Platform carefully. You are also required to follow instructions and regulations issued by CollAction. The foregoing includes (among other things) that you:
+          <ul
+                style={{ listStyleType: "decimal" }}
+                className="pl-8"
+              >
+                <li className="mb-1">
+                  must fill in information truthfully;
+                </li>
+                <li className="mb-1">
+                  may not share misleading or offensive information;
+                </li>
+                <li className="mb-1">
+                  may not perform or permit any acts and/or conduct that violates applicable laws or regulations, public morals, public order or the rights of third parties (including the rights of CollAction);
+                </li>
+                <li className="mb-1">
+                  insofar as you share (personal) data of third parties with us (for example, names, telephone numbers and email addresses), you guarantee that you are authorized to share this data with us in accordance with applicable laws and regulations (for example, because you have permission to do so) and that we can use this data for the purposes for which this data has been shared with us;
+                </li>
+                <li className="mb-1">
+                  may not add to the Platform any texts, data or files that have illegal or offensive content, for example, discriminatory, racist, sexist or pornographic content, or (otherwise) infringe on the private lives of third parties;
+                </li>
+                <li className="mb-1">
+                  must keep your own login data and the personal data of third parties that may be visible to you confidential;
+                </li>
+                <li className="mb-1">
+                  may not spread viruses through the Platform, or otherwise disrupt, prevent or damage (the operation of) the Platform;
+                </li>
+                <li className="mb-1">
+                  must respect and consider the good name and reputation of CollAction; 
+                </li>
+                <li className="mb-1">
+                  must install the application correctly and install updates to the application. Using an older version of the application is at your own risk;
+                </li>
+                <li className="mb-1">
+                  may not sell or rent (the software behind) the Platform to third parties or otherwise make it available to third parties;
+                </li>
+                <li className="mb-1">
+                  may not de-compile, reverse engineer or modify the Platform without CollAction's prior consent. You may also not remove or allow to be removed or circumvent any technical measures intended to protect the Platform.
+                </li>
+              </ul>
+              <br />
+              In the event of (suspected) use of the Platform in violation of these Terms of Use, we may take any action we deem necessary and appropriate, including (but not limited to) denying access to the Platform in whole or in part, temporarily or permanently, and recover any damages and/or costs from you. 
+          </p>
+
+          <h4 className="text-black-300 mb-8">Information posted by third parties</h4>
+          <p className="mb-6 last:mb-0">
+            It is possible for third parties to post texts, data and/or files on the Platform. While we strive to only have content on our Platform that align with CollAction's objectives, texts, data and files posted by third parties are not pre-screened or approved. CollAction is not responsible or liable for any third-party content or materials. CollAction does reserve the right to refuse, remove or permit any texts, data or files at its sole discretion, at any time and for any reason, with or without prior notice, and without any liability towards you or any other third party.
+          </p>
+
+          <h4 className="text-black-300 mb-8">Links to other websites and/or applications</h4>
+          <p className="mb-6 last:mb-0">
+          It is possible that the Platform refers to external internet pages and/or applications, or vice versa. CollAction is not responsible or liable for the use of or the content of these external internet pages and/or applications. 
+          </p>
+
+          <h4 className="text-black-300 mb-8">Liability</h4>
+          <p className="mb-6 last:mb-0">
+          The use of the Platform is at your own risk. We cannot exclude and do not guarantee that the Platform does not or cannot have any defects. Interruptions, errors, viruses, malware or other defects may occur. In addition, the information on the Platform may not be accurate, complete or up to date. CollAction shall not be liable for any damage or injury (including as a result of viruses or malware) resulting from the use of the Platform, unless there is intent or gross negligence on the part of CollAction. The foregoing also means that CollAction is also never liable for indirect or consequential damages, such as loss of sales or income. You indemnify us against all possible third-party claims resulting from the use of the Platform.
+
+          </p>
+
+          <h4 className="text-black-300 mb-8">Intellectual property rights</h4>
+          <p className="mb-6 last:mb-0">
+          Unless otherwise stated, all intellectual property rights and/or similar rights in or to the Platform belong exclusively to CollAction, or our licensors. This includes (among other things) the underlying software, texts, images, videos and audio clips. You may make copies of the information and images on the Platform for your own use, for example by printing or storing them.  
+          <br />
+          <br />
+          The ownership of the texts, data and files placed by you on the Platform remains yours. However, you agree that we can use and distribute these texts, data and files without restriction for any purpose, without acknowledgement and without owing you any compensation, and insofar legally permitted. To the extent necessary, you hereby grant us a free, royalty-free, perpetual, irrevocable, worldwide, non-exclusive, and fully transferable and sub-licensable right and license to use, reproduce, display, distribute, adapt and modify all texts, data and files posted by you, create derivative works from them and otherwise exploit them in any way, commercially or non-commercially.
+          </p>
+
+          <h4 className="text-black-300 mb-8">Privacy and Cookie Policy</h4>
+          <p className="mb-6 last:mb-0">
+          Please be referred to our {" "}
+
+          {<a
+          className="text-collaction"
+          href="https://collaction.org/privacy"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Privacy & Cookie Policy
+        </a>}
+        , which also applies when using the Platform.
+
+          </p>
+
+          <h4 className="text-black-300 mb-8">Choice of law and other provisions</h4>
+          <p className="mb-6 last:mb-0">
+          If these Terms of Use are or become partially invalid, you (and CollAction) will continue to be bound by the remaining, applicable provisions. 
+          <br />
+          <br />
+          The English version of these Terms of Use is binding. Translations are provided for convenience only.
+          <br />
+          <br />
+          These Terms of Use shall be governed by Dutch law. In the event of any dispute relating to these Terms of Use (including the applicability of these Terms of Use), we will try to resolve such dispute together. If that is not possible, the competent court in Amsterdam will have exclusive jurisdiction to settle the dispute.
+          </p>
+          
+          <h4 className="text-black-300 mb-8">Contact Details</h4>
+          <p className="mb-6 last:mb-0">
+          Do you have any questions? Please contact us at {" "}
+          
+          {
+            <a
+          className="text-collaction"
+          href="mailto:terms@collaction.org"
+          target="_blank"
+          rel="noreferrer"
+        >
+          terms@collaction.org
+        </a>
+        }
+          , or by post:
+          <br />
+          <br />
+          Stichting CollAction
+          <br />
+          Hyacinth Avenue 25
+          <br />
+          2015 BA  Amsterdam
+          </p>
         </div>
       </main>
     </>
