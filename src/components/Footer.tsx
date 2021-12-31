73a21ecd83711a0dc5c1f496ce800be536ceb65a
Daniel Strong <danielstrong.ds@gmail.com>
2022-01-01 00:19:23 +0900

    fix(Footer): updated Footer styling and links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,26 +1,108 @@
-import React, { ReactElement } from "react";
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { FiLinkedin, FiInstagram } from "react-icons/fi";
 
-import { InstagramLogo, LinkedinLogo } from 'phosphor-react';
+import CollActionLogoWithText from "public/black-logo-name.png";
+import PoweredByVercel from "public/powered-by-vercel.svg";
 
-export default function Footer(): ReactElement {
+const phone = "+31708888500";
+const email = "contact@collaction.org";
+const KvK = "65628608";
+
+export default function Footer() {
   return (
-    <div className="flex pb-5 px-3 m-auto pt-5 text-gray-800 text-sm flex-col md:flex-row max-w-6xl">
-      <div className="md:w-1/3  mt-2 text-center md:text-left order-3 md:order-1">© Copyright 2021. All Rights Reserved.</div>
+    <div className="bg-secondary text-black-400 py-24 px-5">
+      <div className="flex flex-col md:flex-row md:justify-between w-full md:max-w-600 lg:w-208 lg:max-w-4xl mx-auto">
+        <div>
+          <Link href="/">
+            <a className="block w-40 relative leading-none mb-5">
+              <Image
+                src={CollActionLogoWithText}
+                alt="black CollAction logo with text"
+                layout="responsive"
+                placeholder="blur"
+                sizes="160px"
+              />
+            </a>
+          </Link>
+          <p className="mb-5">
+            © 2022 Copyright <br />
+            All Rights Reserved.
+          </p>
+          <div className="flex mb-5 md:mb-0">
+            <a
+              href="https://www.instagram.com/collaction_org/"
+              target="_blank"
+              rel="noreferrer"
+              className="bg-black-400 text-secondary h-10 w-10 p-2 mr-5 rounded-full"
+              aria-label="Instagram"
+            >
+              <FiInstagram size="24" />
+            </a>
+            <a
+              href="https://www.linkedin.com/company/stichting-collaction"
+              target="_blank"
+              rel="noreferrer"
+              className="bg-black-400 text-secondary h-10 w-10 p-2 mr-5 rounded-full"
+              aria-label="LinkedIn"
+            >
+              <FiLinkedin size="24" />
+            </a>
+          </div>
+        </div>
 
-      <div className="md:w-1/3 text-center order-1 md:order-2 mt-2 flex-row flex justify-center">
-        <a href="https://www.instagram.com/collaction_org/" target="_blank" rel="noreferrer" className="w-6 mx-1">
-          <InstagramLogo size={24} />
-        </a>
-        <a href="https://www.linkedin.com/company/stichting-collaction" target="_blank" rel="noreferrer" className="w-6 mx-1">
-          <LinkedinLogo size={24} />
-        </a>
+        <div>
+          <h5 className="text-headline mb-5">Contact Us</h5>
+          <ul className="list-none">
+            <li className="mb-5">
+              <a href={`tel:${phone}`}>Phone</a>
+            </li>
+            <li className="mb-5">
+              <a href={`mailto:${email}`}>Email</a>
+            </li>
+            <li className="mb-5 md:mb-0">
+              Bezuidenhoutseweg 30 <br />
+              2594 AV Den Haag
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <h5 className="text-headline mb-5">Legal</h5>
+          <ul className="list-none">
+            <li className="mb-5">
+              <Link href="/privacy">
+                <a>Privacy policy</a>
+              </Link>
+            </li>
+            <li className="mb-5">
+              <Link href="/terms">
+                <a>Terms of use</a>
+              </Link>
+            </li>
+            <li className="mb-5">KvK: {KvK}</li>
+            <li className="mb-5 md:mb-0">
+              <Link href="/">
+                <a>ANBI</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
-      
-      <div className="md:w-1/3 order-2 md:order-3 mt-3 mb-1 md:mb-0 md:mt-2 flex-row flex justify-center md:justify-end">
-        <a href="https://vercel.com/?utm_source=collaction&utm_campaign=oss">
-          <img className="block h-6 w-auto" src="/powered-by-vercel.svg" alt="CollAction" />
-        </a>
-      </div>
+
+      <a
+        href="https://vercel.com?utm_source=collaction&utm_campaign=oss"
+        aria-label="Powered By Vercel"
+        className="block w-40 md:mx-auto md:mt-5"
+      >
+        <Image
+          src={PoweredByVercel}
+          alt="Powered by Vercel"
+          layout="responsive"
+          sizes="160px"
+        />
+      </a>
     </div>
   );
 }
