279e1ac5abeb1b083c19642f95fa91111d643616
Mutegetsi prince <mutegetsi2000@gmail.com>
2022-01-18 10:25:58 +0200

    added prince and david to new team

--- testScripts/csvReader.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-import csv
-import json
-
-input_file = csv.DictReader(open("people.csv"))
-output_file = open(Path(__file__).parent / "../src/data" / "new_team.json", "w")
-output_file.write(json.dumps({"members": [row for row in input_file]}))
-output_file.close()
