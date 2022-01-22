2cde6ba41dbb43ad646866557299d594831aea64
Daniel Strong <danielstrong.ds@gmail.com>
2022-01-22 16:16:22 +0900

    fix: updated how deal cards handle their content

--- src/components/DealsCard.tsx
+++ src/components/DealsCard.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Image from 'next/image';
 import clsx from 'clsx';
 import {
@@ -16,7 +16,20 @@ export function DealsCard({
   links,
   content,
 }: Omit<DealsType, 'publish' | 'featured'>) {
+  const contentRef = useRef<HTMLDivElement>(null);
+
   const [showContent, setShowContent] = useState(false);
+  const [contentOverflow, setContentOverflow] = useState(true);
+
+  // used to check if text content is overflowing it's container
+  // if so, we'll show an expand button
+  // saves valuable space on one column layouts
+  useEffect(() => {
+    const contentEl = contentRef.current;
+    if (contentEl) {
+      setContentOverflow(contentEl.scrollHeight - contentEl.clientHeight > 5);
+    }
+  }, []);
 
   return (
     <div className="bg-secondary max-w-400 w-full rounded-3xl overflow-hidden relative mb-8 md:odd:mr-8">
@@ -32,14 +45,14 @@ export function DealsCard({
         />
       </div>
       {/* ICONS */}
-      <div className="px-8 sm:px-10 my-6 sm:my-8 h-10 flex">
+      <div className="px-6 xs:px-8 sm:px-10 my-6 sm:my-8 h-10 flex">
         {links.website && (
           <a
             href={links.website}
             target="_blank"
             rel="noreferrer"
             aria-label="Visit Vendor Site"
-            className="bg-black-400 text-secondary p-2 mr-5 rounded-full"
+            className="bg-black-400 text-secondary p-2 mr-2 xs:mr-5 rounded-full"
           >
             <GlobeSimpleIcon size={24} strokeWidth={2} />
           </a>
@@ -50,7 +63,7 @@ export function DealsCard({
             target="_blank"
             rel="noreferrer"
             aria-label="View Location on Map"
-            className="bg-black-400 text-secondary p-2 mr-5 rounded-full"
+            className="bg-black-400 text-secondary p-2 mr-2 xs:mr-5 rounded-full"
           >
             <MapPinIcon size={24} strokeWidth={2} />
           </a>
@@ -61,7 +74,7 @@ export function DealsCard({
             target="_blank"
             rel="noreferrer"
             aria-label="Phone Vendor"
-            className="bg-black-400 text-secondary p-2 mr-5 rounded-full"
+            className="bg-black-400 text-secondary p-2 mr-2 xs:mr-5 rounded-full"
           >
             <PhoneIcon size={24} strokeWidth={2} />
           </a>
@@ -80,52 +93,59 @@ export function DealsCard({
         </a>
       </div>
       {/* TITLE & CONTENT */}
-      <div className="px-8 sm:px-10">
+      <div className="px-6 xs:px-8 sm:px-10">
         <h4 className="text-black-400 mb-5 sm:mb-8">{title}</h4>
-        <div className="text-black-300 h-32 line-clamp-5 whitespace-pre-line">
+        <div
+          ref={contentRef}
+          className="text-black-300 h-full max-h-32 line-clamp-5 whitespace-pre-line"
+        >
           {content}
         </div>
       </div>
       {/* MAXIMIZE CONTENT ICON */}
       <div className="py-6 sm:py-8 pl-8 pr-5 sm:pl-10 sm:pr-8 text-right">
-        <button
-          onClick={() => setShowContent(true)}
-          className="inline-block bg-black-100 text-secondary p-2 rounded-full"
-        >
-          <PlusIcon size={24} strokeWidth={2} />
-        </button>
+        {contentOverflow && (
+          <button
+            onClick={() => setShowContent(true)}
+            className="inline-block bg-black-100 text-secondary p-2 rounded-full"
+          >
+            <PlusIcon size={24} strokeWidth={2} />
+          </button>
+        )}
       </div>
 
       {/* OPENED CARD */}
-      <div
-        className={clsx(
-          'absolute top-0 left-0 w-full h-full bg-accent text-secondary rounded-3xl transition-opacity',
-          showContent ? 'opacity-100' : 'opacity-0 pointer-events-none'
-        )}
-        style={{
-          transitionDuration: '300ms',
-          transitionTimingFunction: 'cubic-bezier(0,0,0.58,1)',
-        }}
-      >
-        <div className="flex flex-col justify-between h-full">
-          {/* TITLE & CONTENT */}
-          <div className="overflow-auto px-8 sm:px-10 pt-8 flex flex-col">
-            <h5 className="text-title-2 mb-6 sm:mb-8">{title}</h5>
-            <div className="overflow-y-scroll scrollbar-track-accent-500 scrollbar-thumb-secondary whitespace-pre-line">
-              {content}
+      {contentOverflow && (
+        <div
+          className={clsx(
+            'absolute top-0 left-0 w-full h-full bg-accent text-secondary rounded-3xl transition-opacity',
+            showContent ? 'opacity-100' : 'opacity-0 pointer-events-none'
+          )}
+          style={{
+            transitionDuration: '300ms',
+            transitionTimingFunction: 'cubic-bezier(0,0,0.58,1)',
+          }}
+        >
+          <div className="flex flex-col justify-between h-full">
+            {/* TITLE & CONTENT */}
+            <div className="overflow-auto px-8 sm:px-10 pt-8 flex flex-col">
+              <h5 className="text-title-2 mb-6 sm:mb-8">{title}</h5>
+              <div className="overflow-y-scroll scrollbar-track-accent-500 scrollbar-thumb-secondary whitespace-pre-line">
+                {content}
+              </div>
+            </div>
+            {/* MINIMIZE CONTENT ICON */}
+            <div className="py-6 sm:py-8 pl-8 pr-5 sm:pl-10 sm:pr-8 text-right shrink-0">
+              <button
+                onClick={() => setShowContent(false)}
+                className="inline-block bg-secondary text-black-100 p-2 rounded-full"
+              >
+                <XIcon size={24} strokeWidth={2} />
+              </button>
             </div>
           </div>
-          {/* MINIMIZE CONTENT ICON */}
-          <div className="py-6 sm:py-8 pl-8 pr-5 sm:pl-10 sm:pr-8 text-right shrink-0">
-            <button
-              onClick={() => setShowContent(false)}
-              className="inline-block bg-secondary text-black-100 p-2 rounded-full"
-            >
-              <XIcon size={24} strokeWidth={2} />
-            </button>
-          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
