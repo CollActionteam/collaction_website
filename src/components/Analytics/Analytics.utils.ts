2da92f56b05e5004f15f835f96f846004b1f9867
Daniel Strong <danielstrong.ds@gmail.com>
2022-01-23 21:11:37 +0900

    fix: update analytic utils

--- src/components/Analytics/Analytics.utils.ts
+++ src/components/Analytics/Analytics.utils.ts
@@ -19,6 +19,7 @@ export const GA_TRACKING_ID = 'G-8DEJX1KVE0';
 // accept/reject cookies
 export function makeCookieSelection(selection: SelectionType) {
   setCookies(cookieAcceptanceName, selection, cookieAcceptanceOptions);
+  (window as any)[`ga-disable-${GA_TRACKING_ID}`] = false;
 }
 
 // checks if the user has made their cookie selection
@@ -42,6 +43,9 @@ export function revokeAcceptedCookies() {
   makeCookieSelection('0');
   // remove google analytic cookies if they are there
   removeCookies('_ga');
-  removeCookies('_gat');
-  removeCookies('_gid');
+  removeCookies(`_ga_${GA_TRACKING_ID.slice(2)}`);
+  // if analytics were enabled, then disabled, we can set ...
+  // ... this variable to true to stop sending analytics
+  // https://developers.google.com/analytics/devguides/collection/analyticsjs/user-opt-out#opt-out_of_measurement_for_your_site
+  (window as any)[`ga-disable-${GA_TRACKING_ID}`] = true;
 }
