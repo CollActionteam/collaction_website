629430eb7a888c15d3ac5cd694927a9c90272ba7
Daniel Strong <danielstrong.ds@gmail.com>
2022-01-23 23:08:53 +0900

    feat: created custom 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+import PageSEO from 'src/components/PageSEO';
+
+export default function Custom404() {
+  return (
+    <>
+      <PageSEO title="Page Not Found" />
+
+      <main className="bg-black-0 p-5 md:p-12">
+        <div className="max-w-600 mx-auto text-center">
+          <h1 className="text-black-600 mb-6">Page Not Found</h1>
+          <Image
+            unoptimized
+            src="/im-lost.gif"
+            alt="lost dog"
+            width={300}
+            height={395}
+          />
+        </div>
+      </main>
+    </>
+  );
+}
