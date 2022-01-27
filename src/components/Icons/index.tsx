9390f3c7ac90d86d53fdd9301e16de5ad63d8adb
Daniel Strong <danielstrong.ds@gmail.com>
2022-01-27 17:57:12 +0900

    feat: created custom CollAction Icons components

---
+++ src/components/Icons/index.tsx
@@ -0,0 +1,6 @@
+export { default as NoBeef } from './NoBeef';
+export { default as NoCheese } from './NoCheese';
+export { default as NoDairy } from './NoDairy';
+export { default as Pescatarian } from './Pescatarian';
+export { default as Vegan } from './Vegan';
+export { default as Vegetarian } from './Vegetarian';
