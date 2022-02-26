788f565922f892e6ebf323fb7751703f99236cb5
Daniel Strong <danielstrong.ds@gmail.com>
2022-02-26 19:37:58 +0900

    fix: reset the page styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,15 +5,16 @@ export default function HomePage() {
     <>
       <PageSEO title="CollAction | Sustainable choices made easy" />
 
-      <main className="bg-primary-0 p-5 md:p-12">
-        <div className="max-w-600 mx-auto">
-          <h1 className="text-primary-400 text-center mb-6">
-            Privacy and Cookie Policy
-          </h1>
-          <p className="text-primary-400 text-center mb-7 text-body-short-1 rounded-1">
-            version dated January 23rd, 2022
-          </p>
-        </div>
+      <main className="bg-secondary p-5">
+        {/* FIRST CONTAINER */}
+
+        {/* SECOND CONTAINER */}
+
+        {/* THIRD CONTAINER */}
+
+        {/* FOURTH CONTAINER */}
+
+        {/* FIFTH CONTAINER */}
       </main>
     </>
   );
