843d0c9b4e4e75c1bd5c88ead5582d271f576fa8
Daniel <danielstrong.ds@gmail.com>
2022-04-11 10:10:22 -0600

    fix: updated footer spacing

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,8 +13,8 @@ const RSIN = '856193690';
 
 export default function Footer() {
   return (
-    <footer className="bg-secondary text-primary-400 absolute bottom-0 w-full h-940 md:h-440 py-24 px-5">
-      <div className="flex flex-col md:flex-row md:justify-between w-full md:max-w-600 lg:w-208 lg:max-w-4xl mx-auto">
+    <footer className="bg-secondary text-primary-400 absolute bottom-0 w-full h-940 md:h-440 py-12 px-5">
+      <div className="flex flex-col md:flex-row md:justify-between w-full max-w-400 md:max-w-600 lg:w-208 lg:max-w-4xl mx-auto py-12 border-t border-primary-100">
         <div>
           <Link href="/">
             <a className="block w-40 relative leading-none mb-5">
