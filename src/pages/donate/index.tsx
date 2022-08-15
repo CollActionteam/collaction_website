e70ea14f6a31eb846ff9eeffe4b38722ab83cd66
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-08-15 17:45:49 +0100

    feat: add donate page img styles

--- src/pages/donate/index.tsx
+++ src/pages/donate/index.tsx
@@ -1,8 +1,12 @@
 
+import Image from 'next/image';
+
 import PageSEO from 'src/components/PageSEO';
 import InfoCard from 'src/components/InfoCard';
 import AppLinkApple from 'src/components/AppLinkApple';
 import AppLinkGoogle from 'src/components/AppLinkGoogle';
+
+import donateImg from 'public/donate-page-photo.jpg';
 
 export default function DonatePage() {
   return (
@@ -13,7 +17,10 @@ export default function DonatePage() {
       />
 
       <main className="bg-secondary p-5 pt-10 md:pt-11">
-       
+        <div className="mx-auto w-744 rounded-3xl overflow-hidden">
+          <Image src={donateImg} alt="Support us" layout="responsive" />
+        </div>
+
         <InfoCard
           isSecondaryBg
           title="Our individual actions are a drop in the ocean. But together, we make waves!"
