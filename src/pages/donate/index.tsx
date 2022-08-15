038d2b7b4530cf86b7871229f9a763e70a138fa1
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-08-15 21:03:59 +0100

    feat: add donate page headline and text

--- src/pages/donate/index.tsx
+++ src/pages/donate/index.tsx
@@ -1,5 +1,5 @@
-
 import Image from 'next/image';
+import clsx from 'clsx';
 
 import PageSEO from 'src/components/PageSEO';
 import InfoCard from 'src/components/InfoCard';
@@ -13,12 +13,33 @@ export default function DonatePage() {
     <>
       <PageSEO
         title="CollAction | Become a supporter"
-        description="Help us on our mission to make doing fun and easy"
+        description="Help us on our mission to make doing good fun and easy"
       />
 
       <main className="bg-secondary p-5 pt-10 md:pt-11">
-        <div className="mx-auto w-744 rounded-3xl overflow-hidden">
-          <Image src={donateImg} alt="Support us" layout="responsive" />
+        <div className={clsx('relative',
+        'mx-auto rounded-1 overflow-hidden',
+        'md:w-3/4 max-w-350 sm:max-w-400 lg:max-w-864',
+        'mb-12',
+      )}>
+          <Image src={donateImg} alt="collaction team members standing" layout="responsive" />
+          <div className="absolute bottom-22">
+            <h3 className="mb-6 text-secondary">Become a supporter</h3>
+            <p className="text-secondary">Help us on our mission to make doing good fun & easy</p>
+          </div>
+        </div>
+
+        <div className={clsx('relative',
+        'mx-auto md:w-3/4 max-w-350 sm:max-w-400 lg:max-w-864',
+        'mb-12',
+        )}>
+          <h3>Headline</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+            tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+            aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          </p>
         </div>
 
         <InfoCard
