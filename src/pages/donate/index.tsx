c75f6348c027b3e01bf111f30d859181e99d8c00
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-08-26 19:18:54 +0100

    chore: apply prettier fix

--- src/pages/donate/index.tsx
+++ src/pages/donate/index.tsx
@@ -17,28 +17,42 @@ export default function DonatePage() {
       />
 
       <main className="bg-secondary p-5 pt-10 md:pt-11">
-        <div className={clsx('relative',
-        'mx-auto rounded-1 overflow-hidden',
-        'md:w-3/4 max-w-350 sm:max-w-400 lg:max-w-864',
-        'mb-12',
-      )}>
-          <Image src={donateImg} alt="collaction team members standing" layout="responsive" />
+        <div
+          className={clsx(
+            'relative',
+            'mx-auto rounded-1 overflow-hidden',
+            'md:w-3/4 max-w-350 sm:max-w-400 lg:max-w-864',
+            'mb-12'
+          )}
+        >
+          <Image
+            src={donateImg}
+            alt="collaction team members standing"
+            layout="responsive"
+          />
           <div className="absolute bottom-22">
             <h3 className="mb-6 text-secondary">Become a supporter</h3>
-            <p className="text-secondary">Help us on our mission to make doing good fun & easy</p>
+            <p className="text-secondary">
+              Help us on our mission to make doing good fun & easy
+            </p>
           </div>
         </div>
 
-        <div className={clsx('relative',
-        'mx-auto md:w-3/4 max-w-350 sm:max-w-400 lg:max-w-864',
-        'mb-12',
-        )}>
+        <div
+          className={clsx(
+            'relative',
+            'mx-auto md:w-3/4 max-w-350 sm:max-w-400 lg:max-w-864',
+            'mb-12'
+          )}
+        >
           <h3>Headline</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-            tempor incididunt ut labore et dolore magna aliqua.
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
-            aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
-            voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur.
           </p>
         </div>
 
