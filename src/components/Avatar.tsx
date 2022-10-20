54cf5ed7278547cf6fa2d5110f0c9b4500d03cc4
luristran <trangialong105@gmail.com>
2022-10-20 13:15:29 -0400

    Fix: update style and content for profile

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -9,19 +9,24 @@ interface AvatarProps {
 
 export default function Avatar(props: AvatarProps) {
   return (
-    <div className="w-20 hflex flex-col text-center pb-5">
+    <div className="w-20 h-fit flex-col text-center flex flex-col justify-center items-center pb-5">
       <a href={props.linkedin_github}>
         <div className="h-20 overflow-hidden">
           <Image
             src={props.src}
             className="rounded-full"
             alt={props.alt}
-            width="80px"
-            height="80px"
+            width="90px"
+            height="90px"
           />
         </div>
-        <p className="pt-2">{props.alt}</p>
       </a>
+      <p className="md:w-auto sm:w-fit lg:w-fit pt-2 text-lg font-bold text-center leading-5">{props.alt}</p>
+      
+        <div className="">
+        <p className="pt-3 text-sm text-center leading-5">Developer</p>
+        <p className="pt-2  text-sm text-center leading-5">NL</p>
+        </div>
     </div>
   );
 }
