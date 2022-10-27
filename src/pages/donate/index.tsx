5bd49ab6bae993188b6c66a60c10a1f2af34d528
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-10-27 15:50:39 +0100

    refactor: Add font-size styling for byline

--- src/pages/donate/index.tsx
+++ src/pages/donate/index.tsx
@@ -31,12 +31,12 @@ export default function DonatePage() {
         </div>
 
         <div className="mx-auto max-w-600 my-11">
-          <h3 className="mb-7">Headline</h3>
-          <p className="mb-7">
+          <h3 className="mb-7 text-headline-m-1">Headline</h3>
+          <p className="mb-7 text-body-short-1">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </p>
-          <p>
+          <p className="mb-7 text-body-short-1">
             Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
             nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
             reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
