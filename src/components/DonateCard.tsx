9ffb1575fdb1294b8faa0ffb6b05b288d08409e9
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-11-03 16:09:44 +0100

    feat: Add complete boilerplate code for DonateCard component

--- src/components/DonateCard.tsx
+++ src/components/DonateCard.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import Button from 'src/components/Button';
+import DonationAmount from 'src/components/DonationAmount';
 
 interface DonateCardProps {
   headline: string;
   donation: string;
 }
+
+const donations = ['€5.00', '€10.00', '€20.00', '€50.00', '€100.00'];
 
 export default function DonateCard({ headline, donation }: DonateCardProps) {
   return (
@@ -16,7 +19,11 @@ export default function DonateCard({ headline, donation }: DonateCardProps) {
           <p>I will supoort collAction with a {donation} donation</p>
         </div>
         <div>
-          <DonationAmount />
+          <div>
+            {donations.map(donation => {
+              <DonationAmount key={donation} amount={donation} />;
+            })}
+          </div>
           <p>
             Don't have a credit card? No problem. Simply select SEPA Direct
             Debit in the next step.
