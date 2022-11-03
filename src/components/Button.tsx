deb5feb015ce58bd7c2e9504e820002b0f67b41f
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-11-03 15:37:32 +0100

    refactor: Rename Button component interface

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-interface Button {
+interface ButtonProps {
   text: string;
   style: string;
 }
 
-export default function Button({ text, style }: Button) {
+export default function Button({ text, style }: ButtonProps) {
   return (
     <button className={`rounded-lg py-4 px-3 text-sm shadow-xl ${style}`}>
       {text}
