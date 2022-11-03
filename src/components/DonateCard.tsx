af3f4d5917d7eddc380420a38df74c74f38672ae
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-11-03 20:48:55 +0100

    feat: Style the donation component

--- src/components/DonateCard.tsx
+++ src/components/DonateCard.tsx
@@ -8,30 +8,47 @@ interface DonateCardProps {
   donation: string;
 }
 
-const donations = ['€5.00', '€10.00', '€20.00', '€50.00', '€100.00'];
+const donations = [
+  '€5.00',
+  '€10.00',
+  '€20.00',
+  '€50.00',
+  '€100.00',
+  'Other...',
+];
 
 export default function DonateCard({ headline, donation }: DonateCardProps) {
   return (
     <>
-      <div>
-        <div>
-          <h3>{headline}</h3>
-          <p>I will supoort collAction with a {donation} donation</p>
+      <div className="flex justify-between items-center">
+        <div className="flex flex-wrap">
+          <h3 className="w-[319px] h-[34px] mb-7 text-headline-m-1 text-primary-400">
+            {headline}
+          </h3>
+          <p className="text-body-long-1 text-primary-200 w-[242px] h-[48px]">
+            I will support collAction with a {donation} donation
+          </p>
         </div>
-        <div>
-          <div>
-            {donations.map(donation => {
-              <DonationAmount key={donation} amount={donation} />;
-            })}
+        <div className="w-[319px] h-[354px] flex flex-col items-center gap-y-8">
+          <div className="w-[319px] h-[168px] flex flex-wrap gap-3">
+            {donations.map(donation => (
+              <DonationAmount key={donation} amount={donation} />
+            ))}
           </div>
-          <p>
+          <p className="w-[319px] h-[44px] text-sm leading-[22px] text-primary-200">
             Don't have a credit card? No problem. Simply select SEPA Direct
             Debit in the next step.
           </p>
-          <Button text="Continue" style="bg-primary-500" />
-          <p>
-            By donating you agree with our <span>Privacy Policy</span>
-          </p>
+          <div>
+            <Button
+              text="Continue"
+              style="bg-primary-400 w-[319px] h-[48px] leading-[22px] text-secondary rounded-[999px]"
+            />
+            <p className="text-primary-200 text-sm leading-[22px] w-[319px] h-[22px]">
+              By donating you agree with our{' '}
+              <span className="text-collaction-500">Privacy Policy</span>
+            </p>
+          </div>
         </div>
       </div>
     </>
