aab328d0584940a6a3eec6b362488899e8ef3a11
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-11-03 20:49:49 +0100

    feat: Style the donationAmount component

--- src/components/DonationAmount.tsx
+++ src/components/DonationAmount.tsx
@@ -3,5 +3,17 @@ interface DonationAmountProps {
 }
 
 export default function DonationAmount({ amount }: DonationAmountProps) {
-  return <div>{amount}</div>;
+  const active =
+    amount === '€10.00'
+      ? 'bg-collaction-500 text-secondary font-semibold'
+      : 'bg-secondary';
+
+  return (
+    <div
+      className={`w-[153.5px] h-[48px] flex items-center 
+    justify-center rounded-[10px] ${active}`}
+    >
+      {amount}
+    </div>
+  );
 }
