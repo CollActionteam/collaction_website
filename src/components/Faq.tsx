d52bcf46231191ef3a99f7a74afe14bd6069257a
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-11-05 21:11:33 +0100

    feat: Add the faq component

---
+++ src/components/Faq.tsx
@@ -0,0 +1,10 @@
+export default function Faq() {
+  return (
+    <div
+      className="flex items-center w-full h-full gap-2.5
+  py-6 px-3 bg-primary-0 rounded-[10px]"
+    >
+      This is a question
+    </div>
+  );
+}
