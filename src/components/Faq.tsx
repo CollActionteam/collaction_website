39828143ddb56dd6ef54bab3ba2def29a14aa375
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-11-05 21:36:42 +0100

    feat: Update the faq component

--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -1,10 +1,15 @@
+import { FiPlus as Plus } from 'react-icons/fi';
+
 export default function Faq() {
   return (
     <div
       className="flex items-center w-full h-full gap-2.5
   py-6 px-3 bg-primary-0 rounded-[10px]"
     >
-      This is a question
+      <p className="w-[292px] h-[24px] font-light text-body-long-1 text-primary-400">
+        This is a question?
+      </p>
+      <Plus className="w-[24px] h-[24px]" />
     </div>
   );
 }
