ab2f421a9dc97d6ddb016d44214cd29ebf7ac56d
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-11-06 22:04:35 +0100

    feat: Add dummy faq data

--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -52,3 +52,21 @@ export default function Faq({ question, answer }: FaqProps) {
     </div>
   );
 }
+
+export const faq = [
+  {
+    id: 1,
+    question: 'This is a question?',
+    answer: 'This is an answer to a frequently asked question',
+  },
+  {
+    id: 2,
+    question: 'This is a question?',
+    answer: 'This is an answer to a frequently asked question',
+  },
+  {
+    id: 3,
+    question: 'This is a question?',
+    answer: 'This is an answer to a frequently asked question',
+  },
+];
