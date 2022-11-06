dd4f17c07c3ca8a6be329800af5b306cd78ad2de
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-11-06 20:44:22 +0100

    feat: Add Faq accordion functionality

--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -1,18 +1,38 @@
+import { useState } from 'react';
 import { FiPlus as Plus } from 'react-icons/fi';
 
 export default function Faq() {
+  const [isPressed, setIsPressed] = useState(false);
+
+  // toggle FAQ accordion
+  const toggleFaq = () => {
+    setIsPressed(!isPressed);
+  };
+
+  // accordion collapse or expand boolean
+  const hidden = isPressed ? '' : 'hidden';
+
+  // accordion button rotate boolean
+  const rotate = isPressed ? 'rotate-45' : '';
+
   return (
     <div
-      className="flex items-center w-full h-full gap-2.5
+      className="flex flex-col items-center w-full h-full gap-2.5
   py-6 px-3 bg-primary-0 rounded-[10px]"
     >
-      <p
-        className="w-[292px] sm:w-[342px] lg:w-[686px] h-[24px] font-light text-body-long-1 
-      text-primary-400"
-      >
-        This is a question?
-      </p>
-      <Plus className="w-[24px] h-[24px]" />
+      <div className="flex items-center w-full">
+        <p
+          className="w-[292px] sm:w-[342px] lg:w-[686px] font-light 
+        text-body-long-1 text-primary-400"
+        >
+          This is a question?
+        </p>
+        <Plus
+          className={`w-[24px] h-[24px] ${rotate} 
+          transition transform duration-300 ease-in-out`}
+          onClick={toggleFaq}
+        />
+      </div>
     </div>
   );
 }
