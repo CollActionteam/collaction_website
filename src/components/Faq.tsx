0449794e339dde832188f942c1b17a24b11a7f8e
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-11-06 21:31:10 +0100

    feat: Update Faq styles, add Faq props

--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -1,7 +1,12 @@
 import { useState } from 'react';
 import { FiPlus as Plus } from 'react-icons/fi';
 
-export default function Faq() {
+interface FaqProps {
+  question: string;
+  answer: string;
+}
+
+export default function Faq({ question, answer }: FaqProps) {
   const [isPressed, setIsPressed] = useState(false);
 
   // toggle FAQ accordion
@@ -18,20 +23,31 @@ export default function Faq() {
   return (
     <div
       className="flex flex-col items-center w-full h-full gap-2.5
-  py-6 px-3 bg-primary-0 rounded-[10px]"
+  py-6 px-3 bg-primary-0 rounded-[10px] justify-between"
     >
       <div className="flex items-center w-full">
         <p
           className="w-[292px] sm:w-[342px] lg:w-[686px] font-light 
         text-body-long-1 text-primary-400"
         >
-          This is a question?
+          {question}
         </p>
         <Plus
           className={`w-[24px] h-[24px] ${rotate} 
           transition transform duration-300 ease-in-out`}
           onClick={toggleFaq}
         />
+      </div>
+      <div
+        className={`w-full flex items-center mt-6 pt-6 border-t-[0.5px] 
+      border-solid border-primary-100 ${hidden}`}
+      >
+        <p
+          className="w-[326px] sm:w-[376px] lg:w-[720px] font-light 
+        text-body-long-1 text-primary-400"
+        >
+          {answer}
+        </p>
       </div>
     </div>
   );
