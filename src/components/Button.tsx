4ae9e687f8dcf257171116f96343071571f438c5
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-11-07 16:03:20 +0100

    refactor: Add clickHandler button prop

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,13 +3,15 @@ import React from 'react';
 interface ButtonProps {
   text: string;
   style: string;
+  clickHandler?: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-export default function Button({ text, style }: ButtonProps) {
+export default function Button({ text, style, clickHandler }: ButtonProps) {
   return (
     <button
       className={`flex justify-center items-center box-border font-[500] 
       text-sm leading-[22px] h-[48px] shadow-xl ${style}`}
+      onClick={clickHandler}
     >
       {text}
     </button>
