30470f362e9691a2272c4b66f08579156f2a359d
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-11-10 19:29:31 +0100

    feat: Make FAQ element clickable

--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -25,7 +25,13 @@ export default function Faq({ question, answer }: FaqProps) {
       className="flex flex-col items-center h-full gap-2.5
   py-6 px-3 bg-primary-0 rounded-[10px] justify-between max-w-350 sm:max-w-400 lg:max-w-[744px]"
     >
-      <div className="flex items-center w-full">
+      <div
+        className="flex items-center w-full"
+        onClick={toggleFaqHandler}
+        role="button"
+        tabIndex={0}
+        onKeyPress={toggleFaqHandler}
+      >
         <p
           className="w-[292px] sm:w-[342px] lg:w-[686px] font-light 
         text-body-long-1 text-primary-400"
