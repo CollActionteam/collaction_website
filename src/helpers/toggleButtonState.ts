dec4a333d599b624e03cc5f95e901d10b1449175
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-11-11 14:44:53 +0100

    refactor: Update the helper function

--- src/helpers/toggleButtonState.ts
+++ src/helpers/toggleButtonState.ts
@@ -1,14 +1,39 @@
+function toggleActive(
+  element: HTMLButtonElement,
+  activeStyles: string[],
+  inactiveStyles: string[]
+): void {
+  element.classList.add(...activeStyles);
+  element.classList.remove(...inactiveStyles);
+}
+
+function toggleInactive(
+  element: HTMLButtonElement,
+  activeStyles: string[],
+  inactiveStyles: string[]
+): void {
+  element.classList.remove(...activeStyles);
+  element.classList.add(...inactiveStyles);
+}
+
 export function toggleBtnState(
   element: HTMLButtonElement,
   activeStyles: string[],
   inactiveStyles: string[]
 ): void {
   if (element.textContent === 'Recurring donation') {
-    element.classList.add(...activeStyles);
-    element.nextElementSibling?.classList.remove(...activeStyles);
+    toggleActive(element, activeStyles, inactiveStyles);
+    toggleInactive(
+      element.nextElementSibling as HTMLButtonElement,
+      activeStyles,
+      inactiveStyles
+    );
   } else {
-    element.classList.remove(...inactiveStyles);
-    element.classList.add(...activeStyles);
-    element.previousElementSibling?.classList.remove(...activeStyles);
+    toggleActive(element, activeStyles, inactiveStyles);
+    toggleInactive(
+      element.previousElementSibling as HTMLButtonElement,
+      activeStyles,
+      inactiveStyles
+    );
   }
 }
