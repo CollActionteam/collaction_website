986e8dfd4e0ce403b1abd5f10fe92feb43b8cb01
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-11-12 21:20:35 +0100

    refactor: Update the toggleButtonState function

--- src/helpers/toggleButtonState.ts
+++ src/helpers/toggleButtonState.ts
@@ -1,42 +1,22 @@
 // this internal function adds the active class to element
-function toggleActive(
-  element: HTMLButtonElement,
-  activeStyles: string[],
-  inactiveStyles: string[]
-): void {
-  element.classList.add(...activeStyles);
-  element.classList.remove(...inactiveStyles);
+function toggleActive(element: HTMLButtonElement): void {
+  element.classList.add('active-btn');
+  element.classList.remove('inactive-btn');
 }
 
 // this internal function adds the inactive class to element
-function toggleInactive(
-  element: HTMLButtonElement,
-  activeStyles: string[],
-  inactiveStyles: string[]
-): void {
-  element.classList.remove(...activeStyles);
-  element.classList.add(...inactiveStyles);
+function toggleInactive(element: HTMLButtonElement): void {
+  element.classList.remove('active-btn');
+  element.classList.add('inactive-btn');
 }
 
 // this exported function toggles active and inactive btn state
-export function toggleBtnState(
-  element: HTMLButtonElement,
-  activeStyles: string[],
-  inactiveStyles: string[]
-): void {
+export function toggleBtnState(element: HTMLButtonElement): void {
   if (element.textContent === 'Recurring donation') {
-    toggleActive(element, activeStyles, inactiveStyles);
-    toggleInactive(
-      element.nextElementSibling as HTMLButtonElement,
-      activeStyles,
-      inactiveStyles
-    );
+    toggleActive(element);
+    toggleInactive(element.nextElementSibling as HTMLButtonElement);
   } else {
-    toggleActive(element, activeStyles, inactiveStyles);
-    toggleInactive(
-      element.previousElementSibling as HTMLButtonElement,
-      activeStyles,
-      inactiveStyles
-    );
+    toggleActive(element);
+    toggleInactive(element.previousElementSibling as HTMLButtonElement);
   }
 }
