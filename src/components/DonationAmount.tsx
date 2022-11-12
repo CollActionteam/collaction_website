d678743d5e5613daa78d734820633ecb9ae75e8c
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-11-12 21:02:16 +0100

    feat: Add active state style on click or keypress

--- src/components/DonationAmount.tsx
+++ src/components/DonationAmount.tsx
@@ -1,17 +1,30 @@
+import { toggleActiveState } from 'src/helpers/toggleDonationState';
+
 interface DonationAmountProps {
   amount: string;
 }
 
 export default function DonationAmount({ amount }: DonationAmountProps) {
-  const active =
-    amount === '€10.00'
-      ? 'bg-collaction-500 text-secondary font-semibold'
-      : 'bg-secondary';
+  // click handler function
+  const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    // destructure the active element from Event object
+    const { currentTarget: activeAmount } = event;
+    toggleActiveState(activeAmount);
+  };
+
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    const { currentTarget: activeAmount } = event;
+    toggleActiveState(activeAmount);
+  };
 
   return (
     <div
       className={`w-[144px] lg:w-[153.5px] h-[48px] flex items-center 
-    justify-center rounded-[10px] ${active}`}
+    justify-center rounded-[10px] bg-secondary active-donation`}
+      onClick={handleClick}
+      role="button"
+      tabIndex={0}
+      onKeyDown={handleKeyPress}
     >
       {amount}
     </div>
