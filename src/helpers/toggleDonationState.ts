08a806dd4d2eb871932bd4233c747dfb30c82751
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-11-21 12:27:27 +0100

    fix: Set active class by element id

--- src/helpers/toggleDonationState.ts
+++ src/helpers/toggleDonationState.ts
@@ -8,8 +8,7 @@ export function toggleActiveState(element: HTMLInputElement): void {
   // loop through children, add active class to active element
   //remove active class from other element that has active class
   for (const child of children) {
-    const childValue = child.getAttribute('value');
-    if (childValue === element.value) {
+    if (child.id === element.id) {
       child.classList.add('active-donation');
       continue;
     } else {
