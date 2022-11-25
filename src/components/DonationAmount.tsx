f76d32fffb4e09dca9315ce9110f5c93380e5b13
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-11-25 13:32:53 +0100

    fix: Remove merge conflict markers

--- src/components/DonationAmount.tsx
+++ src/components/DonationAmount.tsx
@@ -18,33 +18,26 @@ export default function DonationAmount({
   const handleClick = (event: React.MouseEvent<HTMLInputElement>) => {
     // destructure the active element from Event object
     const { currentTarget: activeAmount } = event;
+    // toggle active state on donation amount
     toggleActiveState(activeAmount);
     if (activeAmount.id === 'Other...') {
+      // if other is clicked, make the input writable
       setIsReadOnly(false);
       if (inputRef.current) inputRef.current.value = '';
     }
     handleDonation(activeAmount.value);
   };
 
-<<<<<<< HEAD
   // change handler function for the user donation input
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { value: currentAmount } = event.currentTarget;
     // might be a good idea to debounce this
     handleDonation(currentAmount);
-=======
-  // keypress handler function
-  const handleKeyPress = (event: React.KeyboardEvent<HTMLDivElement>) => {
-    // destructure the event target from the Event object
-    const { currentTarget: activeAmount } = event;
-    toggleActiveState(activeAmount);
->>>>>>> ce2c12f (fix: Fix active donation bug)
   };
 
   const defaultActiveDonation = amount === '€10.00' ? 'active-donation' : '';
 
   return (
-<<<<<<< HEAD
     <input
       type="text"
       ref={inputRef}
@@ -53,11 +46,6 @@ export default function DonationAmount({
       onChange={handleChange}
       className={`w-[144px] lg:w-[153.5px] h-[48px] flex rounded-[10px] text-center 
       bg-secondary focus:outline-none cursor-pointer ${defaultActiveDonation}`}
-=======
-    <div
-      className={`w-[144px] lg:w-[153.5px] h-[48px] flex items-center 
-    justify-center rounded-[10px] bg-secondary ${defaultActiveDonation}`}
->>>>>>> ce2c12f (fix: Fix active donation bug)
       onClick={handleClick}
       readOnly={isReadOnly}
     />
