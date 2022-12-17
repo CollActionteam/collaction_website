c6bea99b7dcf61c30f1ed9ae1dc0f94ec938acd6
Phoenix-Alpha <betafoenix@gmail.com>
2022-12-17 19:43:12 +0200

    fix accidential remove

--- src/components/Icons/index.tsx
+++ src/components/Icons/index.tsx
@@ -3,3 +3,4 @@ export { default as NoCheese } from './NoCheese';
 export { default as NoDairy } from './NoDairy';
 export { default as Pescatarian } from './Pescatarian';
 export { default as Vegan } from './Vegan';
+export { default as Vegetarian } from './Vegetarian';
