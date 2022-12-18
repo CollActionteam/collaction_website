8f2a0cc9665e47696b50e1eafc7a5b59ff583476
margaretmwaura <mwauramargaret1@gmail.com>
2022-12-18 07:11:31 +0300

    running prettier on file

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,24 +1,25 @@
-
-const range = (start: number , end: number ) => {
-  return [ ...Array(end).keys()].map((el) => el + start);
+const range = (start: number, end: number) => {
+  return [...Array(end).keys()].map(el => el + start);
 };
 
 type paginationItemType = {
-   page: number,
-   currentPage: number,
-   onPageChange: (arg0: any) => void
+  page: number;
+  currentPage: number;
+  onPageChange: (arg0: any) => void;
 };
 
 type PaginationType = {
-  currentPage: any,
-  total: number, 
-  limit: number, 
-  onPageChange: any
-}
+  currentPage: any;
+  total: number;
+  limit: number;
+  onPageChange: any;
+};
 
-const PaginationItem = ({page,
-   currentPage,
-   onPageChange} : paginationItemType) => {
+const PaginationItem = ({
+  page,
+  currentPage,
+  onPageChange,
+}: paginationItemType) => {
   return (
     <button onClick={() => onPageChange(page)} className="">
       <span className="text-collaction-400 p-5">{page}</span>
@@ -26,12 +27,17 @@ const PaginationItem = ({page,
   );
 };
 
-const Pagination = ( {currentPage, total, limit, onPageChange} : PaginationType ) => {
+const Pagination = ({
+  currentPage,
+  total,
+  limit,
+  onPageChange,
+}: PaginationType) => {
   const pagesCount = Math.ceil(total / limit);
   const pages = range(1, pagesCount);
   return (
     <div className="flex">
-      {pages.map((page) => (
+      {pages.map(page => (
         <PaginationItem
           page={page}
           key={page}
