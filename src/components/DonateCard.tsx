bdd3570c100aa2482603cb88c9d7a3cecd8576dd
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-12-20 21:36:16 +0100

    refactor: Refactor donate page and related files (#33)
    * refactor: Refactor donate page and related files
    
    * fix: Fix donate type error and broken media queries
    
    * fix: Add yarn.lock
    
    * feat: Add donate page localization
    
    * fix: Fix toggleButtonState.ts bug

--- src/components/DonateCard.tsx
+++ src/components/DonateCard.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React, { useState } from 'react';
 
 import Button from 'src/components/Button';
@@ -6,6 +7,10 @@ import DonationAmount from 'src/components/DonationAmount';
 interface DonateCardProps {
   headline: string;
   donation: string;
+  creditCardText: string;
+  continueBtnText: string;
+  agreement: string;
+  policy: string;
 }
 
 const donations = [
@@ -17,7 +22,15 @@ const donations = [
   'Other...',
 ];
 
-export default function DonateCard({ headline, donation }: DonateCardProps) {
+export default function DonateCard({
+  headline,
+  donation,
+  creditCardText,
+  continueBtnText,
+  agreement,
+  policy,
+}: DonateCardProps) {
+  // donation amount state variable
   const [donationAmount, setDonationAmount] = useState('');
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
@@ -42,7 +55,7 @@ export default function DonateCard({ headline, donation }: DonateCardProps) {
             className="text-body-long-1 text-primary-200 w-[302px] h-[34px] 
           sm:w-[352px] lg:w-[242px] lg:h-[48px] font-light"
           >
-            I will support collAction with a {donation} donation
+            {donation}
           </p>
         </div>
         <form
@@ -63,18 +76,16 @@ export default function DonateCard({ headline, donation }: DonateCardProps) {
             ))}
           </div>
           <p className="w-[302px] lg:w-[319px] h-[44px] text-sm leading-[22px] text-primary-200">
-            Don't have a credit card? No problem. Simply select SEPA Direct
-            Debit in the next step.
+            {creditCardText}
           </p>
           <div className="w-[302px] flex flex-col items-start gap-2.5">
-            <Button
-              text="Continue"
-              style="bg-primary-400 w-[302px] lg:w-[319px] text-secondary rounded-[999px]"
-            />
+            <Button customStyle="bg-primary-400 w-[302px] lg:w-[319px] text-secondary rounded-[999px]">
+              {continueBtnText}
+            </Button>
             <p className="text-primary-200 text-sm leading-[22px] w-[302px] lg:w-[319px] h-[22px]">
-              By donating you agree with our{' '}
+              {agreement}{' '}
               <span className="text-collaction-500 text-sm leading-[22px]">
-                Privacy Policy
+                <Link href="/privacy">{policy}</Link>
               </span>
             </p>
           </div>
